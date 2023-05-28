44aa9e6a8f080b56e361a24d72e9ed996c2a18db
haozihhr <132857791+haozihhr@users.noreply.github.com>
2023-05-28 15:51:10 +0800

    第二次修复bug

--- docs/static/index.js
+++ docs/static/index.js
@@ -14,7 +14,7 @@ let __Time = 20;
 let __k = 4;
 let _close = false;
 let _fsj = false;
-var url = 'https://haozihhr.github.io/static/image/ClickBefore.png';
+var url = 'https://haozihhr.github.io/eatsadepa/static/image/ClickBefore.png';
 
 function isplaying() {
     return document.getElementById('welcome').style.display == 'none' &&
